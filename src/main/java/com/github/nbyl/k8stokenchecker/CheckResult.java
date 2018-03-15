cf99c5e673185c5da07dddf42bf49f83a04632b0
Nicolas Byl <nico@nicolas-byl.eu>
2018-03-15 13:16:22 +0100

    check ca.crt

--- src/main/java/com/github/nbyl/k8stokenchecker/CheckResult.java
+++ src/main/java/com/github/nbyl/k8stokenchecker/CheckResult.java
@@ -2,13 +2,31 @@ package com.github.nbyl.k8stokenchecker;
 
 public class CheckResult {
 
+    private final boolean success;
+
     private final String path;
 
+    private final String message;
+
     public CheckResult(String path) {
+        this(true, path, "");
+    }
+
+    public CheckResult(boolean success, String path, String message) {
+        this.success = success;
         this.path = path;
+        this.message = message;
+    }
+
+    public boolean isSuccess() {
+        return success;
     }
 
     public String getPath() {
         return path;
+    }
+
+    public String getMessage() {
+        return message;
     }
 }
--- src/main/java/com/github/nbyl/k8stokenchecker/CheckerResource.java
+++ src/main/java/com/github/nbyl/k8stokenchecker/CheckerResource.java
@@ -1,13 +1,41 @@
 package com.github.nbyl.k8stokenchecker;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
 
 @RestController
 public class CheckerResource {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(CheckerResource.class);
+
+    private static final String CA_CRT_PATH = "";
+
     @RequestMapping("/")
     public CheckResult[] checkToken() {
-        return new CheckResult[0];
+        CheckResult[] results = {checkCAFile()};
+        return results;
+    }
+
+    private CheckResult checkCAFile() {
+        try {
+            try (InputStream pemInputStream = new FileInputStream(CA_CRT_PATH)) {
+                CertificateFactory certFactory = CertificateFactory.getInstance("X509");
+                X509Certificate cert = (X509Certificate) certFactory.generateCertificate(pemInputStream);
+            }
+
+            return new CheckResult(CA_CRT_PATH);
+        } catch (CertificateException | IOException e) {
+            LOGGER.debug("Error checking ca.crt", e);
+            return new CheckResult(false, CA_CRT_PATH, org.apache.commons.lang3.exception.ExceptionUtils.getStackTrace(e));
+        }
     }
 }
